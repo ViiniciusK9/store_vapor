c7f8446aef5db1db3905ad8a45db5ed745ad5597
ViiniciusK9 <viniciuskoncicoski@gmail.com>
2021-08-28 16:31:32 -0300

    main

---
+++ user.py
@@ -0,0 +1,13 @@
+import sistema
+
+
+class Cadastro:
+    nome = None
+    email = None
+    senha = None
+    conf_senha = None
+    cpf = None
+
+    
+    def __init__(self) -> None:
+        print('Oi')
