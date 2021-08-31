2ee6bf3bea1d0c51b673e8835f89639536703a35
ViiniciusK9 <viniciuskoncicoski@gmail.com>
2021-08-30 21:01:47 -0300

    main

--- teste.py
+++ teste.py
@@ -1,15 +1,16 @@
 
+def ver_produtos(self,):
 
-produtos = []
+    produtos = []
 
-with open('a_produtos.txt', 'r') as arquivo:
-    for l in arquivo:
-        produtos.append(l.split(','))
-print('='*66)      
-print('|Cód|  |Descrição|                                         |Preço|')
-print('='*66)
-for l in produtos:
-    a = l[2].replace('\n', '')
-    print(f'  {l[0]:<5}{a:<50}{l[1]}')
-print('='*66)
+    with open('a_produtos.txt', 'r') as arquivo:
+        for l in arquivo:
+            produtos.append(l.split(','))
+    print('='*66)      
+    print('|Cód|  |Descrição|                                         |Preço|')
+    print('='*66)
+    for l in produtos:
+        a = l[2].replace('\n', '')
+        print(f'  {l[0]:<5}{a:<50}{l[1]}')
+    print('='*66)
 
