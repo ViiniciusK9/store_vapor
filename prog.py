f4bd6b9e8f2a41f9e5f92534cb965acec1030987
ViiniciusK9 <viniciuskoncicoski@gmail.com>
2021-08-31 22:24:31 -0300

    main

--- prog.py
+++ prog.py
@@ -2,12 +2,22 @@ from os import error
 from user import Cadastro
 
 carrinho = []
-
+produtos = []
 class Store:
     def __init__(self):
+        self.i_produtos()
         self.logo()
         self.menu_inicial()
 
+
+    def i_produtos(self):
+        with open('a_produtos.txt', 'r') as arquivo:
+            for l in arquivo:
+                x = l.split(',')
+                x[2] = x[2].replace('\n', '')
+                x[1] = float(x[1].replace('R$ ', ''))
+                produtos.append(x)
+        
 
     def menu_inicial(self):
         print('''
@@ -31,6 +41,7 @@ class Store:
             else:
                 print('Opção não encontrada.')
 
+
     def menu_loja(self):
         print('''
         [1] - VER PRODUTOS
@@ -44,7 +55,7 @@ class Store:
             self.op = input('Digite uma opção: ')
             
             if self.op == '1':
-                self.ver_produtos()
+                self.ver_produtos(produtos)
             elif self.op == '2':
                 pass
             elif self.op == '3':
@@ -101,19 +112,15 @@ class Store:
         return False
 
     
-    def ver_produtos(self,):
-
-        produtos = []
-
-        with open('a_produtos.txt', 'r') as arquivo:
-            for l in arquivo:
-                produtos.append(l.split(','))
+    def ver_produtos(self, produtos):
+        
         print('='*66)      
         print('|Cód|  |Descrição|                                         |Preço|')
         print('='*66)
-        for l in produtos:
-            a = l[2].replace('\n', '')
-            print(f'  {l[0]:<5}{a:<50}{l[1]}')
+
+        for produto in produtos:
+            print(f'  {produto[0]:<5}{produto[2]:<50}R$ {produto[1]}')
+        
         print('='*66)
 
         print('''
@@ -128,47 +135,18 @@ class Store:
             
             if 1 <= int(self.op) <= 20 :
                 un = int(input(f'[{self.op}] - QUANTIDADE DE UNIDADES: '))
-                self.comprar(int(self.op), un)
+                self.colocar_carrinho(self.op, un, produtos)
             elif self.op == '99':
-                self.ver_carrinho(carrinho)
+                pass
             elif self.op == '0':
                 self.menu_loja()
             else:
                 print('Opção não encontrada.')
 
-    def ver_carrinho(self, vetor):
-        produtos = []
-        sum = 0
-        with open('a_produtos.txt', 'r') as arquivo:
-            for l in arquivo:
-                produtos.append(l.split(','))
-        
-        print('='*66)      
-        print('|Cód|  |Descrição|                                    |un| |Preço|')
-        print('='*66)
-            
-        for u in vetor:
-            for l in produtos:
-                if u[0] == int(l[0]):  
-                    a = l[2].replace('\n', '')
-                    sum += (u[1] * (float(l[1].replace('R$ ', ''))))
-                    print(f'  {l[0]:<5}{a:<48}{u[1]}  {l[1]}')
-        
 
-        print('='*66)      
-        print(f'|Total:                                                 R$ {sum:.2f}|')
-        print('='*66)
+    def colocar_carrinho(self, cod, un, produtos):
         
-
-    def comprar(self, cod, un):
-        existe = 0
-        registro = []
-        for i in carrinho:
-            if i[0] == cod:
-                i[1] += un
-                existe = 1
-
-        if existe == 0:
-            registro.append(cod)
-            registro.append(un)
-            carrinho.append(registro)
+        for produto in produtos:
+            if produto[0] == cod:
+                
+        pass
