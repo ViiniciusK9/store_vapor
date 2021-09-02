e9d8b11a0e360f475e89502c10d531a405d8c73d
ViiniciusK9 <viniciuskoncicoski@gmail.com>
2021-09-01 21:57:27 -0300

    main

--- prog.py
+++ prog.py
@@ -3,6 +3,7 @@ from user import Cadastro
 
 carrinho = []
 produtos = []
+usuario_atual = []
 class Store:
     def __init__(self):
         self.i_produtos()
@@ -46,7 +47,7 @@ class Store:
         print('''
         [1] - VER PRODUTOS
         [2] - VER CARRINHO
-        [3] - PAGAR CONTA
+        [3] - EFETUAR PAGAMENTO
         [0] - LOGOUT
         ''')
         print(self.linha())
@@ -57,9 +58,9 @@ class Store:
             if self.op == '1':
                 self.ver_produtos(produtos)
             elif self.op == '2':
-                pass
+                self.ver_carrinho(carrinho, produtos)
             elif self.op == '3':
-                pass
+                self.efetuar_pagamento(usuario_atual[0][3], usuario_atual[0][4])
             elif self.op == '0':
                 self.menu_inicial()
             else:
@@ -106,6 +107,9 @@ class Store:
         for user in r_user:
             try:
                 if str(user[3].replace('\n', '')) == str(cpf) and str(user[1].replace('\n', '')) == str(senha):
+                    user[4] = user[4].replace('\n', '')
+                    usuario_atual.append(user)
+                    print(user)
                     return True
             except IndexError:
                 pass
@@ -135,18 +139,68 @@ class Store:
             
             if 1 <= int(self.op) <= 20 :
                 un = int(input(f'[{self.op}] - QUANTIDADE DE UNIDADES: '))
-                self.colocar_carrinho(self.op, un, produtos)
+                self.colocar_carrinho(self.op, un, produtos, usuario_atual)
             elif self.op == '99':
-                pass
+                self.ver_carrinho(carrinho, produtos)
             elif self.op == '0':
                 self.menu_loja()
             else:
                 print('Opção não encontrada.')
 
 
-    def colocar_carrinho(self, cod, un, produtos):
-        
+    def colocar_carrinho(self, cod, un, produtos, usuario_atual):
+        saldo = float(usuario_atual[0][4])
+        sum = 0
+        registro = []
         for produto in produtos:
             if produto[0] == cod:
-                
-        pass
+                sum += (produto[1] * un)
+                if sum > saldo:
+                    print(f'Limite do seu saldo foi ultrapassado.')
+                    return False
+                else:
+                    usuario_atual[0][4] = float(usuario_atual[0][4]) - sum
+                    existe = 0
+                    for i in carrinho:
+                        if i[1] == cod:
+                            i[2] += un
+                            existe = 1
+                    if existe == 0:
+                        registro.append(cod)
+                        registro.append(un)
+                        carrinho.append(registro)
+    
+
+    def ver_carrinho(self, carrinho, produtos):
+        
+        sum = 0
+        print('='*66)      
+        print('|Cód|  |Descrição|                                    |un| |Preço|')
+        print('='*66)
+
+        for item in carrinho:
+            for produto in produtos:
+                if item[0] == produto[0]:
+                    sum += item[1] * produto[1]
+                    print(f'  {produto[0]:<5}{produto[2]:<47} {item[1]}  R$ {produto[1]}')
+        
+
+        print('='*66)      
+        print(f'|Total:  {sum:>56.2f}|')
+        print('='*66)
+
+
+    def efetuar_pagamento(self, cpf, novo_saldo):
+        conta = []
+        with open('a_user_register.txt','r') as arquivo:
+            for linha in arquivo:
+                a = linha.split(',')
+                if len(a) < 5:
+                    continue
+                if a[3] == str(cpf):
+                    a[4] = str(novo_saldo) + '\n'
+                conta.append(a)
+
+        with open('a_user_register.txt','w') as arquivo:
+            for c in conta:
+                arquivo.write(str(f'{c[0]},{c[1]},{c[2]},{c[3]},{c[4]}')+'\n')
--- test.py
+++ test.py
@@ -1,41 +1,17 @@
+novo_saldo = '500\n'
 
-vetor = [[20, 2], [17, 1]]
+conta = []
 
+with open('a_user_register.txt','r') as arquivo:
+    for linha in arquivo:
+        a = linha.split(',')
+        if len(a) < 5:
+            continue
+        if a[3] == '15157477007':
+            a[4] = novo_saldo
+            print('batatinha')
+        conta.append(a)
 
-def ver_carrinho(vetor):
-    produtos = []
-    sum = 0
-    with open('a_produtos.txt', 'r') as arquivo:
-        for l in arquivo:
-            produtos.append(l.split(','))
-    
-    print('='*66)      
-    print('|Cód|  |Descrição|                                    |un| |Preço|')
-    print('='*66)
-        
-    for u in vetor:
-        for l in produtos:
-            if u[0] == int(l[0]):  
-                a = l[2].replace('\n', '')
-                sum += (u[1] * (float(l[1].replace('R$ ', ''))))
-                print(f'  {l[0]:<5}{a:<48}{u[1]}  {l[1]}')
-    
-
-    print('='*66)      
-    print(f'|Total:                                                 R$ {sum:.2f}|')
-    print('='*66)
-        
-
-
-
-ver_carrinho(vetor)
-
-
-
-
-'''
-print('='*66)      
-print('|Cód|  |Descrição|                                     |un||Preço|')
-print('='*66)
-print('='*66)
-'''
+with open('a_user_register.txt','w') as arquivo:
+    for c in conta:
+        arquivo.write(str(f'{c[0]},{c[1]},{c[2]},{c[3]},{c[4]}')+'\n')
